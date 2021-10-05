cf6cbfb12eda9b3f743d51b82788ef88a258ace9
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-10-05 19:50:38 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+@RequestMapping("/")
+public String start() {
+	return "index";
+}
+
+}
