fbba1c703e0ac9ee55242c9e1aebe27c52c36791
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-11-29 19:31:46 +0530

    price gets stored in db

--- src/main/java/com/example/demo/dao/RoomRepository.java
+++ src/main/java/com/example/demo/dao/RoomRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entities.Rooms;
+import com.example.demo.entities.User;
 
 
 public interface RoomRepository extends JpaRepository<Rooms, Integer>{
-
 }
